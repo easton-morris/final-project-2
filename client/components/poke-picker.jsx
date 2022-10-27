d112a69e48d1de6da6774a779732096c0c96eb5c
Easton Morris <eastonmorris9@gmail.com>
2022-10-27 06:42:16 +0000

    turn the picker modal into a class to add lifecycle methods'

--- client/components/poke-picker.jsx
+++ client/components/poke-picker.jsx
@@ -7,25 +7,28 @@ import '../scss/styles.scss';
 // eslint-disable-next-line
 import * as bootstrap from 'bootstrap';
 
-export default function PokePicker(props) {
-  return (
-    <div className="modal fade" id="pokePicker" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="pokePickerLabel" aria-hidden="true">
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h1 className="modal-title fs-5" id="pokePickerLabel">Modal title</h1>
-            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
-          </div>
-          <div className="modal-body">
-            ...
-          </div>
-          <div className="modal-footer">
-            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" className="btn btn-primary">Understood</button>
+export default class PokePicker extends React.Component {
+
+  render() {
+    return (
+      <div className="modal fade" id="pokePicker" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="pokePickerLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="pokePickerLabel">Modal title</h1>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+              <div />
+            </div>
+            <div className="modal-body">
+              ...
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary">Understood</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-
-  );
+    );
+  }
 }
