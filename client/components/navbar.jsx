8800499b7f3d1bfb46ebb6513e5a488ad43da0be
Easton Morris <eastonmorris9@gmail.com>
2022-11-16 07:44:01 +0000

    updated some navbar elements, add auth file

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -29,7 +29,7 @@ export default function NavBar(props) {
               </a>
               <ul className='dropdown-menu'>
                 <li><a className='dropdown-item' href='#gym-leaders'>Gym Leaders</a></li>
-                <li><a className='dropdown-item' href='#'>Another action</a></li>
+                <li><a className='dropdown-item' href='#'>Battle User (coming soon)</a></li>
                 <li><hr className='dropdown-divider'/></li>
                 <li><a className='dropdown-item' href='#'>Something else here</a></li>
               </ul>
